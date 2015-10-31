f1bdf5e2ab6fed0df29d8d2a7237b4688a4d8721
Aaron Otis <otis@openmailbox.org>
2015-10-30 18:12:22 -0700

    Updated functions and constant tables

--- labs/assembler/Assembler.c
+++ labs/assembler/Assembler.c
@@ -1,28 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "Assembler.h"
 
 #define MAX_FILENAME 255
 
-const char *instructions[TYPES_ARRAY_SIZE] = {
+const char * const InstructionNames[] = {
+   "add", "addu", "and", "jr", "or", "sll", "sltu", "sra", "srl", "sub",
+   "andiu", "andi", "beq", "bne", "lui", "lw", "ori", "sltiu" "sw", "j",
+   "jal"
+};
 
-}
+const int FunctionCodes[] = {
+   0x20, 0x21, 0x24, 0x08, 0x25, 0x00, 0x2B, 0x03, 0x02, 0x22 
+};
 
-Types FindType(char *) {
+const int OpCodes[] = {
+   0x0C, 0x04, 0x05, 0x0F, 0x23, 0x0D, 0x0B, 0x2B, 0x02, 0x03
+};
+
+Types FindType(char *instruction) {
 }
 
 Types *BuildTypesArray() {
+   int ndx;
+   Types *ret = calloc(TYPES_ARRAY_SIZE, sizeof(Types));
+
+   for (ndx = 0; ndx < TYPES_ARRAY_SIZE; ndx++)
+      ret[ndx].name = (char *)InstructionNames[ndx];
+
+   for (ndx = 0; ndx < R_TYPE_SIZE; ndx++)
+      ret[ndx].data |= FunctionCodes[ndx];
+
+   for (ndx = R_TYPE_SIZE; ndx < TYPES_ARRAY_SIZE; ndx++)
+      ret[ndx].data |= OpCodes[ndx] << BYTE_SHIFT_3;
+
+   return ret;
+}
+
+void FillRegister(Types *type, unsigned int val, int pos) {
+   
 }
 
 int main() {
    char *filename;
-   char *InstructionNames[TYPES_ARRAY_SIZE] = {
-      "add", "addu", "and", "jr", "or", "sll", "sltu", "sra", "srl", "sub",
-      "andiu", "andi", "beq", "bne", "lui", "lw", "ori", "sltiu" "sw", "j",
-      "jal"
-   }
+   Types *TypesTable = BuildTypesArray();
 
    printf("Enter a filename:\n");
-   scanf("%MAX_FILENAMEs", &filename);
+   scanf("%*s", MAX_FILENAME, &filename);
 
    // Read file of instructions
 
@@ -36,4 +62,7 @@ int main() {
 
 
    // Generate object code
+
+
+   free(TypesTable);
 }
--- labs/assembler/Assembler.h
+++ labs/assembler/Assembler.h
@@ -1,7 +1,12 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
-#define TYPES_ARRAY_SIZE = 21
+#define BYTE_SHIFT_3 24
+#define REG_BIT_WIDTH 5
+#define TYPES_ARRAY_SIZE 21
+#define R_TYPE_SIZE 10
+#define I_TYPE_SIZE 9
+#define J_TYPE_SIZE 2
 
 typedef struct Types {
    char *name;
@@ -11,20 +16,21 @@ typedef struct Types {
 typedef struct Label {
    char *name;
    int address;
-};
+} Label;
 
 typedef struct Data {
    int address;
    char *data;
    int width;
    int size;
-}
+} Data;
 
 enum Instructions { ADD, ANDI, OR, ORI, ADDU, ADDIU, SLL, SRL, SRA, SLTU,
- SUB, SLTIU, BEQ, BNE, LW, SW, LUI, J, JR, JAL }
+ SUB, SLTIU, BEQ, BNE, LW, SW, LUI, J, JR, JAL };
 
 Types FindType(char *);
 Types *BuildTypesArray();
+void FillRegister(Types *, unsigned int, int);
 
 
 #endif
