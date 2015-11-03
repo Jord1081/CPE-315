8622c339a9df9aa91510ef7ddedfefccfff10954
Aaron Otis <otis@openmailbox.org>
2015-11-02 23:33:30 -0800

    Updated BuildTables function to build a list of Data structs. Currently broken, but the assignment is now due.

--- labs/assembler/Assembler.c
+++ labs/assembler/Assembler.c
@@ -7,14 +7,15 @@
 #define BYTE_SHIFT_3 24
 #define REG_BIT_WIDTH 5
 #define TYPES_ARRAY_SIZE 21
-#define R_TYPE_SIZE 10
+#define OP_LEN 7
 
+#define R_TYPE_SIZE 10
 #define I_TYPE_SIZE 9
 #define J_TYPE_SIZE 2
-#define OP_LEN 7
-#define WORD_LEN 4
-
 #define BUFF_SIZE 100
+
+#define WORD_LEN 4
+#define BYTE_LEN 1
 
 typedef struct Types {
    char *name;
@@ -73,17 +74,20 @@ void AssemblerInit(AssembleData *data, char *inFile, char *outFile) {
    data->inFile = inFile;
    data->outFile = outFile;
    data->typeTable = BuildTypesArray();
-   data->labelTable = NULL;
 
+   data->labelTable = NULL;
    data->numLabels = 0;
-   data->dataTable = NULL;
+
+   data->dataTable = malloc(sizeof(Data));
    data->numData = 0;
+   data->dataTable[data->numData].size = 0;
+   data->dataTable[data->numData].data = NULL;
 
 }
 
 void BuildTables(AssembleData *data) {
-   char buf[BUFF_SIZE], *line, *token;
-   int loc = 0;
+   char buf[BUFF_SIZE], *line, *token, chr;
+   int loc = 0, inDataSec = 0, ndx, conv;
    FILE *input;
 
    input = fopen(data->inFile, "r");
@@ -93,16 +97,56 @@ void BuildTables(AssembleData *data) {
       line = strtok(buf, "#");
       if (strlen(line) > 1) {
          loc++;
+         if (strstr(line, ".data"))
+            inDataSec++;
          if (strpbrk(line, ":")) {
             token = strtok(line, ":");
 
             // Reallocate space for one more label and add to the end of table.
             data->labelTable = realloc(data->labelTable, sizeof(Label) * 
              ++data->numLabels);
-            data->labelTable[data->numLabels - 1].name = malloc(strlen(token));
+            data->labelTable[data->numLabels - 1].name = malloc(strlen(token) + 1);
 
             strcpy(data->labelTable[data->numLabels - 1].name, token);
             data->labelTable[data->numLabels - 1].address = loc * WORD_LEN;
+
+            if (inDataSec) {
+               data->dataTable = realloc(data->dataTable, sizeof(Data) *
+                ++data->numData);
+               data->dataTable[data->numData - 1].address = loc * WORD_LEN;
+
+               // Determine whether data section is .word or .byte and
+               // advance the char pointer past the token.
+               token = strtok(NULL, ",");
+               if (strstr(token, ".word")) {
+                  data->dataTable[data->numData - 1].width = WORD_LEN;
+                  token = strstr(token, ".word") + strlen(".word");
+               }
+               else {
+                  data->dataTable[data->numData - 1].width = BYTE_LEN;
+                  token = strstr(token, ".byte") + strlen(".byte");
+               }
+
+               token = strtok(NULL, ",");
+               for (ndx = 1; token; ndx++) {
+                  data->dataTable[data->numData - 1].data = realloc(
+                   data->dataTable[data->numData - 1].data, 
+                   data->dataTable[data->numData - 1].width * ndx + 1);
+
+                  if (data->dataTable[data->numData - 1].width == WORD_LEN) {
+                     conv = atoi(token);
+                     memcpy(data->dataTable[data->numData - 1].data + ndx,
+                      (char *)&conv, WORD_LEN);
+                  }
+                  else {
+                     chr = atoi(token);
+                     memcpy(data->dataTable[data->numData - 1].data + ndx,
+                      &chr, BYTE_LEN);
+                  }
+                  token = strtok(NULL, ",");
+                  data->dataTable[data->numData - 1].size++;
+               }
+            }
          }
       }
    }
@@ -117,8 +161,20 @@ void Cleanup(AssembleData *data) {
 }
 
 void CheckTables(AssembleData data) {
-   int ndx;
+   int ndx, iNdx;
    for (ndx = 0; ndx < data.numLabels; ndx++)
       printf("Label: %s Address: %d\n", data.labelTable[ndx].name,
        data.labelTable[ndx].address);
+
+   for (ndx = 0; ndx < data.numData; ndx++) {
+      for (iNdx = 0; iNdx < data.numLabels && data.labelTable[iNdx].address 
+       != data.dataTable[ndx].address; iNdx++) {
+         ;
+      }
+      printf("Data: %s: ", data.labelTable[iNdx].name);
+      for (iNdx = 0; iNdx < data.dataTable[ndx].size; iNdx += 
+       data.dataTable[ndx].width)
+         printf("%d, ", data.dataTable[ndx].data[iNdx]);
+      printf("\n");
+   }
 }
